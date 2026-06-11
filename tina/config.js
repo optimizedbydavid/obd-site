b9b9edeaf1efa9f6920ba4803030b934ff0c811a
OptimizedByDavid <david@optimizedbydavid.com>
2026-06-10 18:28:34 -0700

    add tina cms

---
+++ tina/config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "tinacms";
+
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+          },
+          {
+            type:
